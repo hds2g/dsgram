8be9f8384275da10d45d9bc1207cb6b200098e27
DS Hwang <hds2g22@gmail.com>
2020-02-17 07:45:27 +0900

    Add middleware in exprss of graphql server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 import { GraphQLServer } from "graphql-yoga";
+import logger from "morgan";
 
 const PORT = process.env.PORT || 4000;
 
@@ -16,6 +17,7 @@ const resolvers = {
 };
 const server = new GraphQLServer({ typeDefs, resolvers });
 
+server.express.use(logger("dev"));
 server.start({ port: PORT }, () =>
   console.log(`Server running on http://localhost:${PORT}`)
 );

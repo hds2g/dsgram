1b2e3e5ba358259969b0b1aabf629259237e2287
DS Hwang <hds2g22@gmail.com>
2020-02-17 19:00:18 +0900

    #2.3 Integrating Prisma in our Server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
-require("dotenv").config();
+import dotenv from "dotenv";
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
 import schema from "./schema";
 
-const PORT = process.env.PORT || 4000;
+dotenv.config();
+
+const PORT = process.env.PORT;
 
 const server = new GraphQLServer({ schema });
 

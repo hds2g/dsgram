3e88d3e7a127b1c7faa9f3c25cbccaa1a6e2a5f7
DS Hwang <hds2g22@gmail.com>
2020-02-26 07:43:53 +0900

    #3.17 isLiked Computed File

--- src/api/User/computed.js
+++ src/api/User/computed.js
@@ -31,5 +31,23 @@ export default {
       const { id: parentId } = parent;
       return user.id === parentId;
     }
+  },
+  Post: {
+    isLiked: async (parent, _, { request }) => {
+      const { user } = request;
+      const { id } = parent;
+      return prisma.$exists.like({
+        AND: [
+          {
+            user: {
+              id: user.id
+            }
+          },
+          {
+            post: { id }
+          }
+        ]
+      });
+    }
   }
 };

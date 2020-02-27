d94a14e854a524f35966c760c0b9197b65a0ebb2
DS Hwang <hds2g22@gmail.com>
2020-02-28 07:16:38 +0900

    #3.21 deletePost Resolver part Two

--- src/api/Post/editPost/editPost.js
+++ src/api/Post/editPost/editPost.js
@@ -10,13 +10,14 @@ export default {
       const { user } = request;
       const post = await prisma.$exists.post({ id, user: { id: user.id } });
       if (post) {
-        if (action === EDIT)
+        if (action === EDIT) {
           return prisma.updatePost({
             data: { caption, location },
             where: { id }
           });
-      } else if (action === DELETE) {
-        return prisma.deletePost({ id });
+        } else if (action === DELETE) {
+          return prisma.deletePost({ id });
+        }
       } else {
         throw Error("You can't do that");
       }
